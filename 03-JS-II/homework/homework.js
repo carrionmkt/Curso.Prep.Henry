b2729a3b72f42447411688d78ede18b7720bd079
Developer Ar <carrionmkt@gmail.com>
2021-02-18 19:08:19 -0300

    Homework js2

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -117,7 +117,7 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
-  if ( Math.floor (esEntero) === esEntero) {
+  if ( Math.floor (numero) === numero) {
     return true;
   } else {
     return false;
@@ -131,6 +131,15 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
   //Aca va mi codigo
+  if (numero % 3 === 0 && numero % 5 === 0 ){
+    return "fizzbuzz" ;
+  } if (numero % 5 === 0) {
+    return "buzz" ;
+  } if (numero % 3 === 0) {
+    return "fizz" ;
+  } else {
+    return numero ;
+  }
 }
 
 function operadoresLogicos(num1, num2, num3) {
@@ -140,6 +149,18 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
+ 
+  if (num1 === 0 || num2 === 0 || num3 === 0) {
+    return "Error";
+  } if (num1 < 0 || num2 < 0 || num3 < 0 ){
+    return "Hay negativos";
+  } if (num3 > num1 && num3 > num2){
+    return num3 + 1 ;
+  } if (num1 > num2 && num1 > num3 && num1 > 0) {
+    return "Número 1 es mayor y positivo";
+  } else {
+    return false ;
+  }
 }
 
 function esPrimo(numero) {
@@ -148,7 +169,19 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
-}
+  
+  if (numero === 0 || numero === 1){
+    return false;
+  }  for (i = 2 ; i < numero; i++ ) {
+     if (numero % i === 0) {
+       return false;
+       } 
+       else {
+         return true;
+       }
+  }
+    
+ }
 
 function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
